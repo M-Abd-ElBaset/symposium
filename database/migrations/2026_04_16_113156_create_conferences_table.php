089aabe07b1a86d5dfbe9831b283a6d0ba503cb5
Mohamed Elgebaly <36784871+M-Abd-ElBaset@users.noreply.github.com>
2026-04-22 21:40:15 +0200

    added Api integration for importing conferences (cfps) daily

--- database/migrations/2026_04_16_113156_create_conferences_table.php
+++ database/migrations/2026_04_16_113156_create_conferences_table.php
@@ -14,13 +14,14 @@ return new class extends Migration
         Schema::create('conferences', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('location');
-            $table->text('description');
-            $table->string('url');
             $table->date('starts_at');
             $table->date('ends_at');
             $table->date('cfp_starts_at');
             $table->date('cfp_ends_at');
+            $table->string('location')->nullable();
+            $table->string('url')->nullable();
+            $table->text('description')->nullable();
+            $table->string('callingallpapers_id')->nullable();
             $table->timestamps();
         });
     }
