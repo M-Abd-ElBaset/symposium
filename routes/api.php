102783847b4f8d4ec5ae8bdbd0b65fbebbe9ddc1
Mohamed Elgebaly <36784871+M-Abd-ElBaset@users.noreply.github.com>
2026-05-13 10:04:42 +0300

    added pulbic_api speakers with rate limiter and collection resources

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Resources\SpeakerResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/speakers', function (){
-   return 'speakers';
+Route::middleware(['throttle:public_api'])->group(function () {
+    Route::get('/speakers', function (){
+       return SpeakerResource::collection(User::all());
+    });
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 });
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
